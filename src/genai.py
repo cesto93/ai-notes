364c472f4bf3dc53e3450f18faecd4253a2241f1
Pier Francesco Contino <pfcontino@gmail.com>
2026-02-24 21:30:24 +0100

    feat: remove note metadata

--- src/genai.py
+++ src/genai.py
@@ -4,19 +4,6 @@ from langchain_core.language_models.chat_models import BaseChatModel
 from langchain.chat_models import init_chat_model
 
 
-class NoteMetadata(BaseModel):
-    """Data model for note metadata extracted from text."""
-
-    Title: str = Field(
-        ...,
-        description="The title of the note. This should be a concise word categorizing the note",
-    )
-    Directory: str = Field(
-        ...,
-        description="The directory where the note should be stored. This should be a single word representing the category of the note (e.g. 'work', 'personal', 'ideas', 'learning').",
-    )
-
-
 def get_model(provider: str, model: str) -> BaseChatModel:
     """
     Returns the chat model based on provider and model name.
@@ -82,38 +69,6 @@ def paraphrase_text(llm: BaseChatModel, text: str) -> str:
     prompt = prompt_template.invoke({"text": text})
     result = llm.invoke(prompt)
     return result.content
-
-
-def extract_metadata(llm: BaseChatModel, text: str) -> NoteMetadata:
-    """
-    Extracts metadata from the given text using the provided language model.
-
-    Args:
-        llm (ChatGoogleGenerativeAI): The language model to use for metadata extraction.
-        text (str): The text from which to extract metadata.
-
-    Returns:
-        NoteMetadata: An instance containing the extracted metadata.
-    """
-    prompt_template = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are an expert extraction algorithm. "
-                "Only extract relevant information from the text. "
-                "If you do not know the value of an attribute asked to extract, "
-                "return null for the attribute's value.",
-            ),
-            ("human", "{text}"),
-        ]
-    )
-
-    structured_llm = llm.with_structured_output(schema=NoteMetadata)
-    prompt = prompt_template.invoke({"text": text})
-    if isinstance(result, NoteMetadata):
-        return result
-    else:
-        raise ValueError("The result is not of type NoteMetadata.")
 
 
 def generate_mindmap(llm: BaseChatModel, text: str) -> str:
